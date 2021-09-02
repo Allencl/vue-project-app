c7fa8bb66e8d66a443a7e44f781516939b826e62
Allencl <Allen__cc@163.com>
2021-09-02 16:31:50 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router';
+import ViewUI from 'view-design';
 
+import 'view-design/dist/styles/iview.css';
+
+import App from './App.vue'
+import router from './router/config.js'
+
+
+Vue.use(VueRouter);
+Vue.use(ViewUI);
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
