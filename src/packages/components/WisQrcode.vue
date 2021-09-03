597ae23efbc27f28abe542477292b23288c68ac4
Allencl <Allen__cc@163.com>
2021-09-03 16:41:29 +0800

    1

--- src/packages/components/WisQrcode.vue
+++ src/packages/components/WisQrcode.vue
@@ -1,7 +1,26 @@
 <template>
-    <span>
-        <qrcode-stream @decode="onDecode"></qrcode-stream>
-    </span>
+    <div 
+        class="wis-qrcode-stream"
+        :style="`height:${documentHeight}px`"
+    >
+        <qrcode-stream @decode="onDecode" @init="onInit"></qrcode-stream>
+
+        <Icon 
+            class="close-icon"
+            type="md-close-circle" 
+            @click="closeHandle"
+        />
+
+        <Affix :offset-bottom="20">
+            <div style="text-align:center;">
+                <span>请扫描二维码</span>
+            </div>
+        </Affix>
+
+
+        <div class="move"></div>
+
+    </div>
 </template>
 <script>
 import { QrcodeStream} from 'vue-qrcode-reader';
@@ -12,20 +31,27 @@ export default {
     },    
     data() {
         return {
- 
+            documentHeight:document.documentElement.clientHeight,  // 窗口高度
         }
     },        
     created() {
 
     },  
     methods: {
-        onDecode:function(url) {
-            this.$Notice.open({
-                title: 'Notification title',
-                desc: url,
-            });            
+        closeHandle:function(){
+            this.$emit("close");
         },
-
+        onDecode:function(url) {
+            this.$emit("decode",url);
+        },
+        onInit:function(promise) {
+            promise.then(
+                // console.log
+            )
+            .catch(
+                // console.error
+            )
+        }
     },
     props: {
                  
@@ -34,5 +60,46 @@ export default {
 </script>
 
 <style lang="less">
+.wis-qrcode-stream{
+    position: fixed;
+    top: 0px;
+    width: 100%;
+    z-index: 11;
+    // height: 100px;
 
+    .close-icon{
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        color: #fff;
+        z-index: 11;
+        font-size: 26px;
+    }
+
+
+    @keyframes move { 
+        0% {             
+            top:10%;     
+        }            
+        50%{                
+            top:90%;            
+        }            
+        100%{                
+            top:10%; 
+        }
+    }
+
+    .move{
+        position: fixed;
+        top:0px;
+        z-index: 11;
+        animation: move 3s infinite;
+
+        width: 100%;
+        height: 1px;
+        // background: #19be6b;
+        box-shadow: 2px 2px 3px #19be6ba8;
+    }
+
+}
 </style>
--- view/receiving/index.vue
+++ view/receiving/index.vue
@@ -1,104 +1,110 @@
 <template>
-    <div style="padding:12px 8px;">
-        <WisQrcode />
-        <Row style="margin-bottom:28px;margin-top:12px">
-            <i-col span="16">
-                <Input 
-                    v-model="valueInput" 
-                    placeholder="请扫描或输入单号" 
-                    style="width:100%;font-size:12px" 
-                    size="small"
-                />
-            </i-col>
-            <i-col span="8" style="text-align:right">
-                <Button @click="readHandle" type="success" size="small" style="margin-right:8px">读取</Button>
-                <Button @click="scanHandle" type="info" size="small">扫描</Button>
-
-            </i-col>
-        </Row>
-
-
-        <h5 style="text-align:left;padding-bottom:6px">送货单信息</h5>
-        <Form 
-            ref="formInline" 
-            :model="formInline" 
-            :rules="ruleInline" 
-            :label-colon="true"
-            label-position="top"
-        >
-            <Row>
-                <i-col span="12">
-                    <FormItem label="送货单号">
-                        <Input v-model="formInline.odd" size="small" disabled></Input>
-                    </FormItem>            
+    <span>
+        <WisQrcode 
+            v-if="showCamera"
+            @decode="decodeHandle"
+            @close="closeHandle"
+        />
+        <div style="padding:12px 8px;">
+            <Row style="margin-bottom:28px;margin-top:12px">
+                <i-col span="16">
+                    <Input 
+                        v-model="valueInput" 
+                        placeholder="请扫描或输入单号" 
+                        style="width:100%;font-size:12px" 
+                        size="small"
+                    />
                 </i-col>
-                <i-col span="12">
-                    <FormItem label="发货时间">
-                        <Input v-model="formInline.deliverTime" size="small" disabled></Input>
-                    </FormItem>             
+                <i-col span="8" style="text-align:right">
+                    <Button @click="readHandle" type="success" size="small" style="margin-right:8px">读取</Button>
+                    <Button @click="scanHandle" type="info" size="small">扫描</Button>
+
                 </i-col>
             </Row>
-            <Row>
-                <i-col span="12">
-                    <FormItem label="供应商">
-                        <Input v-model="formInline.principal" size="small" disabled></Input>
-                    </FormItem>            
-                </i-col>
-                <i-col span="12">
-                    <FormItem label="到货时间">
-                        <Input v-model="formInline.arrivalTime" size="small" disabled></Input>
-                    </FormItem>             
-                </i-col>
-            </Row>
-            <Row>
-                <i-col span="12">
-                    <FormItem label="卸货负责人">
-                        <Input v-model="formInline.principal" size="small" disabled></Input>
-                    </FormItem>            
-                </i-col>
-                <i-col span="12">
-                    <FormItem label="卸货口电话">
-                        <Input v-model="formInline.phone" size="small" disabled></Input>
-                    </FormItem>             
-                </i-col>
-            </Row>  
-            <Row>
-                <i-col span="12">
-                    <FormItem label="仓库">
-                        <Input v-model="formInline.warehouse" size="small" disabled></Input>
-                    </FormItem>            
-                </i-col>
-                <i-col span="12">
-                    <FormItem label="状态">
-                        <Input v-model="formInline.status" size="small" disabled></Input>
-                    </FormItem>             
-                </i-col>
-            </Row>                      
-        </Form>
 
 
-        <Table 
-            size="small"
-            :columns="columns" 
-            :data="data"
-            style="margin-top:12px"
-        ></Table>
+            <h5 style="text-align:left;padding-bottom:6px">送货单信息</h5>
+            <Form 
+                ref="formInline" 
+                :model="formInline" 
+                :rules="ruleInline" 
+                :label-colon="true"
+                label-position="top"
+            >
+                <Row>
+                    <i-col span="12">
+                        <FormItem label="送货单号">
+                            <Input v-model="formInline.odd" size="small" disabled></Input>
+                        </FormItem>            
+                    </i-col>
+                    <i-col span="12">
+                        <FormItem label="发货时间">
+                            <Input v-model="formInline.deliverTime" size="small" disabled></Input>
+                        </FormItem>             
+                    </i-col>
+                </Row>
+                <Row>
+                    <i-col span="12">
+                        <FormItem label="供应商">
+                            <Input v-model="formInline.principal" size="small" disabled></Input>
+                        </FormItem>            
+                    </i-col>
+                    <i-col span="12">
+                        <FormItem label="到货时间">
+                            <Input v-model="formInline.arrivalTime" size="small" disabled></Input>
+                        </FormItem>             
+                    </i-col>
+                </Row>
+                <Row>
+                    <i-col span="12">
+                        <FormItem label="卸货负责人">
+                            <Input v-model="formInline.principal" size="small" disabled></Input>
+                        </FormItem>            
+                    </i-col>
+                    <i-col span="12">
+                        <FormItem label="卸货口电话">
+                            <Input v-model="formInline.phone" size="small" disabled></Input>
+                        </FormItem>             
+                    </i-col>
+                </Row>  
+                <Row>
+                    <i-col span="12">
+                        <FormItem label="仓库">
+                            <Input v-model="formInline.warehouse" size="small" disabled></Input>
+                        </FormItem>            
+                    </i-col>
+                    <i-col span="12">
+                        <FormItem label="状态">
+                            <Input v-model="formInline.status" size="small" disabled></Input>
+                        </FormItem>             
+                    </i-col>
+                </Row>                      
+            </Form>
 
 
-        <Affix :offset-bottom="20">
-            <div style="text-align:right;padding-right:12px">
-                <Button @click="handleSubmit" type="primary" shape="circle" style="padding:0px;font-size:12px;height: 42px;width: 42px;">
-                    收货
-                </Button>
-            </div>
-        </Affix>
-    </div>
+            <Table 
+                size="small"
+                :columns="columns" 
+                :data="data"
+                style="margin-top:12px"
+            ></Table>
 
+
+            <Affix :offset-bottom="20">
+                <div style="text-align:right;padding-right:12px">
+                    <Button @click="handleSubmit" type="primary" shape="circle" style="padding:0px;font-size:12px;height: 42px;width: 42px;">
+                        收货
+                    </Button>
+                </div>
+            </Affix>
+        </div>
+    </span>
 </template>
 <script>
     export default {
         data () {
             return {
+                showCamera:false,   // 相机
                 valueInput:'',  // 扫码
                 formInline: {
                     odd:'23322aqq',
@@ -171,6 +177,19 @@
         },
         methods: {
             /**
+             * 关闭
+             */
+            closeHandle:function(){
+                this.showCamera=false;
+            },
+            /**
+             * 扫码成功
+             */
+            decodeHandle:function(code){
+                alert(code);
+                this.showCamera=false;
+            },
+            /**
              * 读取
              */
             readHandle:function(){
@@ -180,7 +199,7 @@
              * 扫描
              */
             scanHandle:function(){
-
+                this.showCamera=true;
             },
             /**
              * 收货
